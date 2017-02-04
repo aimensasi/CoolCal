12992c68b1fbce9c2de9a72f9e39a3681fe5241a
aimensasi <ali.sasi@ymail.com>
2017-02-04 15:21:49 +0800

    fixed a bug with the clear button

--- app/src/main/java/com/example/math/coolcalc/CalcActivity.java
+++ app/src/main/java/com/example/math/coolcalc/CalcActivity.java
@@ -2,6 +2,7 @@ package com.example.math.coolcalc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CalcActivity extends Activity {
 
@@ -9,5 +10,6 @@ public class CalcActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
+
     }
 }
